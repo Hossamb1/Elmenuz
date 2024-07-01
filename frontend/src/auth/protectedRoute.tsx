d3ab44088c1731528c37b441e0f9c6ba04f3831a
Hossamb1 <hossambarakat87@gmail.com>
2024-07-01 12:24:47 +0300

    Auth

--- frontend/src/auth/protectedRoute.tsx
+++ frontend/src/auth/protectedRoute.tsx
@@ -1,9 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 export const ProtectedRoute = () => {
   const { isAuthenticated } = useAuth0();
-  const navigate = useNavigate();
 
-  return isAuthenticated ? <Outlet /> : navigate("/"), (<></>);
+  if (!isAuthenticated) return <Navigate to="/" />;
+
+  return <Outlet />;
 };
