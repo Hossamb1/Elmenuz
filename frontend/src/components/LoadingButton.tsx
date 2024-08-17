270e0347d5296bc27d48fd88c777c6b65391254b
Hossamb1 <hossambarakat87@gmail.com>
2024-08-17 16:03:54 +0300

    fixing bugs

--- frontend/src/components/LoadingButton.tsx
+++ frontend/src/components/LoadingButton.tsx
@@ -2,9 +2,9 @@ import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
 
 type Props = {
-  classname: string;
+  classname?: string;
 };
-const LoadingButton = ({ classname }: Props) => {
+const LoadingButton = ({ classname = "" }: Props) => {
   return (
     <Button disabled className={classname}>
       <Loader2 className="mr-2 h-4 w-4 animate-spin" />
